959108ba1f543d33e7e129d9299f18c0f034bfdb
Baiano-Indiano <99291603+Baiano-Indiano@users.noreply.github.com>
2022-06-09 20:53:56 -0300

    09/06/2022

--- js/Game.js
+++ js/Game.js
@@ -7,6 +7,8 @@ class Game {
 
     this.leader1 = createElement("h2");
     this.leader2 = createElement("h2");
+    
+    this.playerMoving = false;
   }
 
   getState() {
@@ -40,6 +42,7 @@ class Game {
 
     fuels = new Group();
     powerCoins = new Group();
+
     obstacles = new Group();
 
     var obstaclesPositions = [
@@ -62,21 +65,30 @@ class Game {
 
     // Adicionar sprite de moeda no jogo
     this.addSprites(powerCoins, 18, powerCoinImage, 0.09);
-    this.addSprites(obstacles, obstaclesPositions.length, obstacle1Image, 0.04, obstaclesPositions);
+
+    //Adicionar sprite de obstáculo no jogo
+    this.addSprites(
+      obstacles,
+      obstaclesPositions.length,
+      obstacle1Image,
+      0.04,
+      obstaclesPositions
+    );
   }
 
   addSprites(spriteGroup, numberOfSprites, spriteImage, scale, positions = []) {
     for (var i = 0; i < numberOfSprites; i++) {
       var x, y;
-      if(positions.length > 0 ){
+
+      //C41 //SA
+      if (positions.length > 0) {
         x = positions[i].x;
         y = positions[i].y;
         spriteImage = positions[i].image;
-      }   else{
-          x = random(width / 2 + 150, width / 2 - 150);
-          y = random(-height * 4.5, height - 400);
+      } else {
+        x = random(width / 2 + 150, width / 2 - 150);
+        y = random(-height * 4.5, height - 400);
       }
-     
       var sprite = createSprite(x, y);
       sprite.addImage("sprite", spriteImage);
 
@@ -91,7 +103,7 @@ class Game {
     form.titleImg.class("gameTitleAfterEffect");
 
     //C39
-    this.resetTitle.html("Reiniciar o Jogo");
+    this.resetTitle.html("Reiniciar Jogo");
     this.resetTitle.class("resetText");
     this.resetTitle.position(width / 2 + 200, 40);
 
@@ -109,29 +121,18 @@ class Game {
     this.leader2.position(width / 3 - 50, 130);
   }
 
-handleFuel(index){
-  cars[index-1].overlap(fuels, function(collector, collected){
-  collected.remove();
-  player.fuel = 185;
-  });
-}
-
-handlePowerCoins(index){
-  cars[index-1].overlap(powerCoins, function(collector, collected){
-  collected.remove();
-  player.score += 21;
-  player.update();
-  });
-}
   play() {
     this.handleElements();
     this.handleResetButton();
 
     Player.getPlayersInfo();
+    player.getCarsAtEnd();
 
     if (allPlayers !== undefined) {
       image(track, 0, -height * 5, width, height * 6);
 
+      this.showFuelBar();
+      this.showLife();
       this.showLeaderboard();
 
       //índice da matriz
@@ -151,15 +152,33 @@ handlePowerCoins(index){
           stroke(10);
           fill("red");
           ellipse(x, y, 60, 60);
+
           this.handleFuel(index);
           this.handlePowerCoins(index);
+
           //alterar a posição da câmera na direção y
           camera.position.y = cars[index - 1].position.y;
         }
       }
 
+      if(this.playerMoving){
+        player.positionY += 5;
+        player.update();
+      }
+
       //manipulando eventos de teclado
       this.handlePlayerControls();
+
+      //Linha de chegada
+      const finshLine = height * 6 - 100;
+
+      if (player.positionY > finshLine) {
+        gameState = 2;
+        player.rank += 1;
+        Player.updateCarsAtEnd(player.rank);
+        player.update();
+        this.showRank();
+      }
 
       drawSprites();
     }
@@ -170,10 +189,33 @@ handlePowerCoins(index){
       database.ref("/").set({
         playerCount: 0,
         gameState: 0,
-        players: {}
+        players: {},
+        carsAtEnd: 0
       });
       window.location.reload();
     });
+  }
+
+  showLife() {
+    push();
+    image(lifeImage, width / 2 - 130, height - player.positionY - 400, 20, 20);
+    fill("white");
+    rect(width / 2 - 100, height - player.positionY - 400, 185, 20);
+    fill("#f50057");
+    rect(width / 2 - 100, height - player.positionY - 400, player.life, 20);
+    noStroke();
+    pop();
+  }
+
+  showFuelBar() {
+    push();
+    image(fuelImage, width / 2 - 130, height - player.positionY - 350, 20, 20);
+    fill("white");
+    rect(width / 2 - 100, height - player.positionY - 350, 185, 20);
+    fill("#ffc400");
+    rect(width / 2 - 100, height - player.positionY - 350, player.fuel, 20);
+    noStroke();
+    pop();
   }
 
   showLeaderboard() {
@@ -222,6 +264,7 @@ handlePowerCoins(index){
   handlePlayerControls() {
     if (keyIsDown(UP_ARROW)) {
       player.positionY += 10;
+      this.playerMoving = true;
       player.update();
     }
 
@@ -234,5 +277,60 @@ handlePowerCoins(index){
       player.positionX += 5;
       player.update();
     }
+  }
+
+  handleFuel(index) {
+    
+    cars[index - 1].overlap(fuels, function(collector, collected) {
+      player.fuel = 185;
+      //o sprite é coletado no grupo de colecionáveis que desencadeou
+      //o evento
+      collected.remove();
+    });
+    if(player.fuel > 0 && this.playerMoving){
+      player.fuel -= 0.3;
+
+    }
+    // reduzindo o combustível do carro
+    if(player.fuel <= 0){
+      gameState = 2;
+      this.gameOver();
+    }
+    
+    
+    
+  }
+
+  handlePowerCoins(index) {
+    cars[index - 1].overlap(powerCoins, function(collector, collected) {
+      player.score += 21;
+      player.update();
+      //o sprite é coletado no grupo de colecionáveis que desencadeou
+      //o evento
+      collected.remove();
+    });
+  }
+
+  showRank() {
+    swal({
+      //title: `Incrível!${"\n"}Rank${"\n"}${player.rank}`,
+      title: `Incrível!${"\n"}${player.rank}º lugar`,
+      text: "Você alcançou a linha de chegada com sucesso!",
+      imageUrl:
+        "https://raw.githubusercontent.com/vishalgaddam873/p5-multiplayer-car-race-game/master/assets/cup.png",
+      imageSize: "100x100",
+      confirmButtonText: "Ok"
+    });
+  }
+
+  gameOver() {
+    swal({
+      title: `Fim de Jogo`,
+      text: "Oops você perdeu a corrida!",
+      imageUrl:
+        "https://cdn.shopify.com/s/files/1/1061/1924/products/Thumbs_Down_Sign_Emoji_Icon_ios10_grande.png",
+      imageSize: "100x100",
+      confirmButtonText: "Obrigado por jogar"
+    });
   }
 }
